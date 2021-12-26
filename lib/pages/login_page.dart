349ca18d95387b2fe479d9e3010aef4733645568
Subhajit Gorai <subhajitthisside@gmail.com>
2021-12-26 15:34:12 +0530

    Day7 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,56 +12,78 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      Navigator.pushNamed(context, MyRoutes.home);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/login_image.png",
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 10),
-              Text("Welcome $name",
-                  style: TextStyle(
-                      color: Colors.indigo[600],
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold)),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                          labelText: "Username",
-                          hintText: "Enter your username"),
-                      onChanged: (value) {
-                        name = value;
-                        setState(() {});
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          labelText: "Password",
-                          hintText: "Enter your password"),
-                    ),
-                  ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/login_image.png",
+                  fit: BoxFit.cover,
                 ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, MyRoutes.home);
-                },
-                child: Text("Login"),
-                style: ElevatedButton.styleFrom(primary: Colors.indigo[600]),
-              )
-            ],
+                SizedBox(height: 10),
+                Text("Welcome $name",
+                    style: TextStyle(
+                        color: Colors.indigo[600],
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold)),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "Username",
+                            hintText: "Enter your username"),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "UserName can't be empty.";
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        },
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: "Password",
+                            hintText: "Enter your password"),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password can't be empty.";
+                          } else if (value.length < 6) {
+                            return "Password must be at least 6 characters";
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => moveToHome(context),
+                  child: Text("Login"),
+                  style: ElevatedButton.styleFrom(primary: Colors.indigo[600]),
+                )
+              ],
+            ),
           ),
         ));
   }
