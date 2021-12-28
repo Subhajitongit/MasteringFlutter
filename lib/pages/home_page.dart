26966a17af4ccd41852293676abf97f032758413
Subhajit Gorai <subhajitthisside@gmail.com>
2021-12-28 23:14:19 +0530

    Beautiful UI design

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,14 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, unused_local_variable, non_constant_identifier_names, unnecessary_null_comparison
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, unused_local_variable, non_constant_identifier_names, unnecessary_null_comparison, prefer_typing_uninitialized_variables, unused_import
 
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:master_flutter/models/catalog.dart';
+import 'package:master_flutter/utils/theme.dart';
 import 'package:master_flutter/widgets/drawer.dart';
 import 'package:master_flutter/widgets/item_widget.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -37,21 +39,104 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Catalog app",
-            style: TextStyle(color: Colors.black),
+      backgroundColor: MyTheme.creamcolor,
+      body: SafeArea(
+        child: Container(
+          padding: Vx.m16,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              CatalogHeader(),
+              if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+                CatalogList().expand()
+              else
+                Center(child: CircularProgressIndicator())
+            ],
           ),
         ),
-        body: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? Padding(
-                padding: const EdgeInsets.all(16),
-                child: ListView.builder(
-                  itemCount: CatalogModel.items.length,
-                  itemBuilder: (context, index) =>
-                      ItemWidget(item: CatalogModel.items[index]),
-                ))
-            : Center(child: CircularProgressIndicator()),
-        drawer: MyDrawer());
+      ),
+    );
+  }
+}
+
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl5.bold.color(MyTheme.darkBluish).make(),
+        "Trending Products".text.make()
+      ],
+    );
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: CatalogModel.items.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.items[index];
+        return CatalogItem(catalog: catalog);
+      },
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final catalog;
+  const CatalogItem({Key? key, @required this.catalog}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String name = catalog.name;
+    String desc = catalog.desc;
+    return VxBox(
+        child: Row(
+      // ignore: prefer_const_literals_to_create_immutables
+      children: [
+        CatalogImage(
+          image: catalog.image,
+        ),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            name.text.lg.color(MyTheme.darkBluish).bold.make(),
+            desc.text.caption(context).make(),
+            ButtonBar(alignment: MainAxisAlignment.spaceBetween, children: [
+              "\$${catalog.price}".text.bold.xl.make(),
+              ElevatedButton(
+                onPressed: () {},
+                child: "Buy".text.make(),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(MyTheme.darkBluish),
+                    shape: MaterialStateProperty.all(StadiumBorder())),
+              )
+            ])
+          ],
+        ))
+      ],
+    )).white.rounded.square(150).make().py16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+  const CatalogImage({Key? key, required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image).box.make().p12();
   }
 }
--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
   static ThemeData get lightThemeData => ThemeData(
       primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0,
@@ -13,4 +15,8 @@ class MyTheme {
   static ThemeData get darkThemeData => ThemeData(
         brightness: Brightness.dark,
       );
+
+//colors
+  static Color creamcolor = Color(0xfff5f5f5);
+  static Color darkBluish = Color(0xff40385b);
 }
